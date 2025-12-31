1385ff72c9d149f4cf1251c1b3d057824fe09ad8
SnowFlash383935 <snowflash383935@mail.ru>
2025-12-31 13:50:17 +0300

    Update and rename ping.py to ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -6,7 +6,7 @@ It also includes other timestamps to help calculate the coldstart time.
 import time
 import discohook
 
-@discohook.command.slash('ping', description = 'Ping test the bot (modified)!')
+@discohook.command.slash('ping', description = 'Ping test the bot!')
 async def ping_command(interaction):
   created_at = interaction.created_at
   now = time.time()
