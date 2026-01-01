5aad1b842dd85ed716439cc02b52a31da35355f0
SnowFlash383935 <snowflash383935@mail.ru>
2026-01-01 16:21:11 +0300

    Update ping.py

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -5,6 +5,7 @@ It also includes other timestamps to help calculate the coldstart time.
 
 import time
 import discohook
+import datetime
 
 @discohook.command.slash('пинг', description = 'Пинг бота!')
 async def ping_command(interaction):
@@ -15,9 +16,9 @@ async def ping_command(interaction):
   text = '\n'.join([
     'Понг! Задержка: `{:.2f}ms`'.format(since * 1000),
     '',
-    'Бот запущен в: {}'.format(interaction.client.started_at.timestamp()),
-    'Interaction создана в: {}'.format(created_at),
-    'Текущее время: {}'.format(now)
+    'Бот запущен в: {}'.format(str(interaction.client.started_at)),
+    'Interaction создана в: {}'.format(str(created_at)),
+    'Текущее время: {}'.format(str(datetime.datetime.utcnow()))
   ])
 
   await interaction.response.send(text)
